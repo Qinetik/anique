8b2499faffef2b89f2603fe18e5163c523fdef59
Waqas Tahir <wakaztahir@gmail.com>
2023-10-01 16:33:18 +0500

    updates

--- src/solid/components/AppBar.tsx
+++ src/solid/components/AppBar.tsx
@@ -3,8 +3,9 @@ import MenuIcon from "../icons/Menu";
 import {Row} from "@qinetik/anique/row";
 import {ThemeSwitcher} from "./ThemeSwitcher";
 import {AppBar} from "@qinetik/anique/appbar";
+import {GithubIconBlack, GithubIconWhite} from "../icons/GithubIcons.tsx";
 
-export function WebAppBar(props : { title ?: string }) {
+export function WebAppBar(props: { title?: string }) {
     return (
         <AppBar
             navIcon={
@@ -16,8 +17,14 @@ export function WebAppBar(props : { title ?: string }) {
             }
             title={props.title || "Anique"}
             actions={(
-                <Row>
-                    <ThemeSwitcher />
+                <Row gap={"1em"}>
+                    <IconButton as={"a"} target={"_blank"} href={"https://github.com/qinetik/anique"} class={"display-in-dark"}>
+                        <GithubIconWhite/>
+                    </IconButton>
+                    <IconButton as={"a"} target={"_blank"} href={"https://github.com/qinetik/anique"} class={"display-in-light"}>
+                        <GithubIconBlack/>
+                    </IconButton>
+                    <ThemeSwitcher/>
                 </Row>
             )}
         />
