a867c117520bdaa2623502970237d46bda23b705
Waqas Tahir <wakaztahir@gmail.com>
2023-10-06 14:32:03 +0500

    new build using rollup, instead of tsup

---
+++ packages/anique-icons/rollup.config.js
@@ -0,0 +1,40 @@
+import babel from "@rollup/plugin-babel";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import terser from '@rollup/plugin-terser';
+
+const extensions = [".js", ".ts", ".jsx", ".tsx"]
+
+/**
+ * @type import("rollup").RollupOptions
+ */
+function createConfiguration(solidGenerate, format, name, extension) {
+    return (
+        {
+            input: {
+                file: "src/index.tsx"
+            },
+            output: {
+                dir: "dist",
+                format : format,
+                entryFileNames : name + "." + extension
+            },
+            plugins: [
+                babel({
+                    presets: [
+                        "@babel/preset-typescript",
+                        ["solid", {"generate": solidGenerate, "hydratable": true}]
+                    ],
+                    extensions: extensions
+                }),
+                nodeResolve({extensions}),
+                terser()
+            ],
+            external : ["solid-js", "solid-js/web", "solid-js/store"]
+        }
+    )
+}
+
+export default [
+    createConfiguration("dom", "es", "index", "js"),
+    createConfiguration("ssr", "cjs", "index","cjs"),
+]
