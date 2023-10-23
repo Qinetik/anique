09cefc5393b082cc415b9f7bcca67d71512c4a75
Waqas Tahir <wakaztahir@gmail.com>
2023-10-23 13:59:40 +0500

    added additional icons

--- packages/anique-icons/src/AlertIcon.tsx
+++ packages/anique-icons/src/AlertIcon.tsx
@@ -1,5 +1,7 @@
-export default function AlertIcon() {
+import {JSX} from "solid-js";
+
+export default function AlertIcon(props : JSX.SvgSVGAttributes<SVGSVGElement>) {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>alert</title><path d="M13 14H11V9H13M13 18H11V16H13M1 21H23L12 2L1 21Z" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}><title>alert</title><path d="M13 14H11V9H13M13 18H11V16H13M1 21H23L12 2L1 21Z" /></svg>
     )
 }
