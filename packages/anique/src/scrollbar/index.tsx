b5409c1beb3d079236dd8f7af54e4c2c3619fb56
Waqas Tahir <wakaztahir@gmail.com>
2023-10-23 12:58:10 +0500

    made scrollbar styles, scrollbar area for better control

--- packages/anique/src/scrollbar/index.tsx
+++ packages/anique/src/scrollbar/index.tsx
@@ -1,23 +1,27 @@
-import {mountedStyles} from "@qinetik/emotion";
+import {mountedStyles, styled} from "@qinetik/emotion";
 import {Anique} from "../theme/Theme";
 
-export const ScrollbarStyles = mountedStyles`
+export const ScrollbarArea = styled("div")`
 
-    :root::-webkit-scrollbar-track {
+    &::-webkit-scrollbar-track {
         -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
         background-color: ${Anique.colors.bg200};
         border-radius: 10px;
-        
     }
-    
-    :root::-webkit-scrollbar {
-        width: 6px;
+
+    &::-webkit-scrollbar {
+        width: 7px;
         background-color: ${Anique.colors.bg200};
-    }
-    
-    :root::-webkit-scrollbar-thumb {
-        background-color: ${Anique.colors.bg400};
         border-radius: 10px;
     }
 
+    &::-webkit-scrollbar-thumb {
+        background-color: ${Anique.colors.bg500};
+        border-radius: 10px;
+    }
+
+    &::-webkit-scrollbar-thumb:hover {
+        background-color: ${Anique.colors.onBg500};
+    }
+    
 `
