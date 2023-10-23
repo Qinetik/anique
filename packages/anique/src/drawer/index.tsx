784ffcc0b520b402a2199255af76f5353ebefd60
Waqas Tahir <wakaztahir@gmail.com>
2023-10-23 14:38:44 +0500

    static drawer that used as prop fixed

--- packages/anique/src/drawer/index.tsx
+++ packages/anique/src/drawer/index.tsx
@@ -1,47 +1,46 @@
 import {styled} from "@qinetik/emotion";
-import {Accessor, JSX, splitProps} from "solid-js";
+import {splitProps} from "solid-js";
 import {Anique} from "../theme/Theme";
 
 export const StaticDrawer = styled("div")`
-  display: flex;
-  flex-direction: column;
-  gap: 1em;
-  width: 280px;
-  box-sizing: border-box;
-  padding: 1.5em;
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+    width: 280px;
+    box-sizing: border-box;
+    padding: 1.5em;
 `
 
 export interface DrawerItemProps {
-    isActive ?: boolean
+    isActive?: boolean
 }
 
 export const DrawerItem = styled("div")<DrawerItemProps>`
-  width: 100%;
-  box-sizing: border-box;
-  padding: 0.6em 0.5em 0.6em 1em;
-  border-radius: 999px;
-  cursor: pointer;
-  font-family:${Anique.font.secondary};
-  color : ${Anique.colors.onBg};
-  ${p => p.isActive && "background : " + Anique.colors.bg200 + ";"}
-    
-  &:hover {
-    background: ${Anique.colors.primary};
-  }
+    width: 100%;
+    box-sizing: border-box;
+    padding: 0.6em 0.5em 0.6em 1em;
+    border-radius: 999px;
+    cursor: pointer;
+    font-family: ${Anique.font.secondary};
+    color: ${Anique.colors.onBg};
+
+    ${p => p.isActive && "background : " + Anique.colors.bg200 + ";"}
+    &:hover {
+        background: ${Anique.colors.primary};
+    }
 `
 
-const StyledSectionText = styled("span")`
-  box-sizing: border-box;
-  display:inline-block;
-  padding: 0.6em 0.5em 0.6em 1em;
-  margin : 1em 0 0.25em 0;
-  font-family : ${Anique.font.primary};
+const StyledSectionText = styled("h4")`
+    box-sizing: border-box;
+    display: inline-block;
+    padding: 0.6em 0.5em 0.6em 1em;
+    margin: 1em 0 0.25em 0;
+    font-family: ${Anique.font.primary};
 `
 
 export const DrawerSectionItem = styled((props: { title: string }) => {
-    const split = splitProps(props, ["title"])
     return (
-        <StyledSectionText color={"onBg100"} as={"h4"} children={props.title} {...split[1]}/>
+        <StyledSectionText color={"onBg100"} children={props.title} {...splitProps(props, ["title"])[1]}/>
     )
 })`
 `
