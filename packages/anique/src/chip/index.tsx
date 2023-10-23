be8eaad6d70c929c6372f917651c6c9ab2e2b28d
Waqas Tahir <wakaztahir@gmail.com>
2023-10-23 12:59:02 +0500

    added clickable chip & variation prop instead of background

--- packages/anique/src/chip/index.tsx
+++ packages/anique/src/chip/index.tsx
@@ -2,14 +2,45 @@ import {styled} from "@qinetik/emotion";
 import {Anique, BgColors, BrandColors, ResultColors} from "../theme";
 
 export interface ChipProps {
-    background ?: keyof BgColors | keyof ResultColors | keyof BrandColors
+    variation?: "bg" | "primary" | "accent"
+    background?: keyof BgColors | keyof ResultColors | keyof BrandColors
 }
 
 export const Chip = styled("div")<ChipProps>`
-    display: inline-block;
+    display: flex;
+    flex-direction: row;
+    gap: 0.35em;
+    align-items: center;
     padding: 0.35rem;
     font-size: 0.8rem;
     color: ${Anique.colors.onBg100};
     border-radius: ${Anique.border.mdRadius};
-    background : ${pg=> Anique.colors[pg.background || "bg100"]};
+    background: ${pg =>
+        // @ts-ignore
+        Anique.colors[((pg.variation || "bg") + "100")]
+    };
+    user-select: none;
+`
+
+export const ClickableChip = styled(Chip)`
+
+    cursor: pointer;
+
+    &:hover {
+        background: ${p =>
+            // @ts-ignore
+            Anique.colors[((p.variation || "bg") + "300")]
+        };
+    }
+
+    &:active {
+        background: ${p =>
+            // @ts-ignore
+            Anique.colors[((p.variation || "bg") + "400")]
+        };
+    }
+
+    &.selected {
+        background: ${Anique.colors.primary}
+    }
 `
--- src/solid/routes/ChipPage.tsx
+++ src/solid/routes/ChipPage.tsx
@@ -1,5 +1,7 @@
-import {Card, Column, Headline, Row} from "@qinetik/anique";
-import {Chip} from "@qinetik/anique/chip";
+import {Card, Column, Headline, IconButton, Row} from "@qinetik/anique";
+import {Chip, ClickableChip} from "@qinetik/anique/chip";
+import Menu from "../icons/Menu.tsx";
+import {AlertIcon, ChevronUp} from "@qinetik/anique-icons";
 
 export default function ChipPage() {
     return (
@@ -17,11 +19,58 @@ export default function ChipPage() {
             <h1>Color Variations</h1>
             <Card>
                 <Row gap={"1em"}>
-                    <Chip background={"bg400"}>My First Chip</Chip>
-                    <Chip background={"primary"}>My Second Chip</Chip>
-                    <Chip background={"accent"}>My Third Chip</Chip>
-                    <Chip background={"success"}>My Fourth Chip</Chip>
-                    <Chip background={"warning"}>My Fifth Chip</Chip>
+                    <Chip variation={"bg"}>Bg Chip</Chip>
+                    <Chip variation={"primary"}>Primary Chip</Chip>
+                    <Chip variation={"accent"}>Accent Chip</Chip>
+                </Row>
+            </Card>
+            <h1>Chip with Icons</h1>
+            <Card>
+                <Row gap={"1em"}>
+                    <Chip>
+                        <IconButton size={-0.5}>
+                            <ChevronUp />
+                        </IconButton>
+                        <span>My First Chip</span>
+                    </Chip>
+                    <Chip>
+                        <span>My Last Chip</span>
+                        <IconButton size={-0.5} style={{ transform : "rotate(180deg)" }}>
+                            <ChevronUp />
+                        </IconButton>
+                    </Chip>
+                    <Chip>
+                        <IconButton size={-0.5}>
+                            <ChevronUp />
+                        </IconButton>
+                        <span>My Last Chip</span>
+                        <IconButton size={-0.5} style={{ transform : "rotate(180deg)" }}>
+                            <ChevronUp />
+                        </IconButton>
+                    </Chip>
+                </Row>
+            </Card>
+            <h1>Clickable Chip</h1>
+            <Card>
+                <Row gap={"1em"}>
+                    <ClickableChip background={"primary"}>
+                        <span>My First Chip</span>
+                    </ClickableChip>
+                    <ClickableChip variation={"primary"}>
+                        <span>My First Chip</span>
+                    </ClickableChip>
+                    <ClickableChip variation={"accent"}>
+                        <span>My First Chip</span>
+                    </ClickableChip>
+                    <ClickableChip>
+                        <IconButton size={-0.5}>
+                            <ChevronUp />
+                        </IconButton>
+                        <span>My Last Chip</span>
+                        <IconButton size={-0.5} style={{ transform : "rotate(180deg)" }}>
+                            <ChevronUp />
+                        </IconButton>
+                    </ClickableChip>
                 </Row>
             </Card>
         </Column>
