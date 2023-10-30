95294d765532505929b2f727d4b6f2474c6aeb73
Waqas Tahir <wakaztahir@gmail.com>
2023-10-30 14:41:03 +0500

    headline docs

---
+++ src/routes/components/headline/examples/HeadlineExample.tsx
@@ -0,0 +1,13 @@
+import {Column} from "@qinetik/anique/column";
+import {Headline} from "@qinetik/anique/headline";
+import {Size} from "@qinetik/anique/theme/Size";
+
+export default function HeadlineExample() {
+    return (
+        <Column>
+            <Headline size={Size.Small}>My Small Headline</Headline>
+            <Headline size={Size.Medium}>My Medium Headline</Headline>
+            <Headline size={Size.Large}>My Large Headline</Headline>
+        </Column>
+    )
+}
--- src/routes/components/headline/index.tsx
+++ src/routes/components/headline/index.tsx
@@ -3,24 +3,15 @@ import {Column} from "@qinetik/anique/column";
 import {Headline} from "@qinetik/anique/headline";
 import {Size} from "@qinetik/anique/theme/Size";
 import {DocContainer} from "../../../components/common/layout/DocContainer";
+import {ComponentExample} from "../../../solid/components/ComponentExample";
+import HeadlineExample from "./examples/HeadlineExample";
 
 export default function HeadlinePage() {
     return (
         <DocContainer>
             <Column>
                 <h1>Headlines</h1>
-                <h2>Small Headline</h2>
-                <Card>
-                    <Headline size={Size.Small}>My Small Headline</Headline>
-                </Card>
-                <h2>Medium Headline</h2>
-                <Card>
-                    <Headline size={Size.Medium}>My Medium Headline</Headline>
-                </Card>
-                <h3>Large Headline</h3>
-                <Card>
-                    <Headline size={Size.Large}>My Large Headline</Headline>
-                </Card>
+                <ComponentExample component={HeadlineExample} codeVisible={true} />
             </Column>
         </DocContainer>
     )
