50ea3bcaa479502c37137484dbf13f9eeab42392
Waqas Tahir <wakaztahir@gmail.com>
2023-11-02 10:37:05 +0500

    anique update, dropdown menu fix

--- packages/anique/src/dropdown/menu.tsx
+++ packages/anique/src/dropdown/menu.tsx
@@ -3,6 +3,7 @@ import {Column} from "../column";
 import {Accessor, JSX, ParentProps, splitProps} from "solid-js";
 import {mergeStylesAndVisibility, onClickOutsideProp} from "../backdrop";
 import {AniqueZIndex} from "../utils/zIndexMap";
+import {Portal} from "solid-js/web";
 
 export const DropdownMenuRoot = styled(Column)`
     align-items: unset;
@@ -29,20 +30,22 @@ export function DropdownMenu(props: ParentProps<DropdownMenuProps> & JSX.HTMLAtt
             >
                 {props.children}
             </DropdownMenuRoot>
-            <div
-                {...onClickOutsideProp(props.onCloseRequest)}
-                style={{
-                    width: "100%",
-                    height: "100%",
-                    position: "fixed",
-                    top: 0,
-                    right: 0,
-                    bottom: 0,
-                    left: 0,
-                    "z-index" : AniqueZIndex.DropdownBackdrop,
-                    ...(props.isVisible ? ({ display: props.isVisible() ? "block" : "none" }) : {})
-                }}
-            />
+            <Portal>
+                <div
+                    {...onClickOutsideProp(props.onCloseRequest)}
+                    style={{
+                        width: "100%",
+                        height: "100%",
+                        position: "fixed",
+                        top: 0,
+                        right: 0,
+                        bottom: 0,
+                        left: 0,
+                        "z-index" : AniqueZIndex.DropdownBackdrop,
+                        ...(props.isVisible ? ({ display: props.isVisible() ? "block" : "none" }) : {})
+                    }}
+                />
+            </Portal>
         </>
     )
 }
