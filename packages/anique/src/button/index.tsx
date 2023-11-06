210273bbc22c248cff7fd2426b9a06ac28ff368e
Waqas Tahir <wakaztahir@gmail.com>
2023-11-06 17:56:48 +0500

    small thing

--- packages/anique/src/button/index.tsx
+++ packages/anique/src/button/index.tsx
@@ -1,16 +1,15 @@
-import {styled, useTheme} from "@qinetik/emotion";
+import {styled} from "@qinetik/emotion";
 import {Anique} from "../theme/Theme";
 import {Size} from "../theme/Size";
-import {JSX} from "solid-js";
 
 const ButtonBase = styled("button", {
-    shouldForwardProp : (prop) => prop != "size"
+    shouldForwardProp: (prop) => prop != "size"
 })<{
-    size ?: Size
+    size?: Size
 }>`
 
     box-sizing: border-box;
-    padding: ${p=> 0.8 + (p.size ? (p.size * 0.25) : 0)}em ${p=> 0.8 + (p.size ? (p.size * 0.5) : 0)}em;
+    padding: ${p => 0.8 + (p.size ? (p.size * 0.25) : 0)}em ${p => 0.8 + (p.size ? (p.size * 0.5) : 0)}em;
     background: ${Anique.colors.primary};
     border-radius: ${Anique.border.lgRadius};
     cursor: pointer;
@@ -20,10 +19,10 @@ const ButtonBase = styled("button", {
     overflow: hidden;
 
     &:disabled {
-        background : ${Anique.colors.bg500};
-        cursor : default;
+        background: ${Anique.colors.bg500};
+        cursor: default;
     }
-    
+
     &:hover:not(&:disabled) {
         background: ${Anique.colors.primary100};
     }
